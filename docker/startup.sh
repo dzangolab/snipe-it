9be00dd4e5cd5072b164c0007e7e408fc4e3b1ad
Olivier Pichon <op@dzangolab.com>
2021-05-02 15:12:23 +0700

    feat(docker): add support for docker secrets

--- docker/startup.sh
+++ docker/startup.sh
@@ -1,5 +1,20 @@
 #!/bin/bash
 
+if [ -n "${APP_KEY_FILE}" ]; then
+  APP_KEY=$(cat "$APP_KEY_FILE")
+  export APP_KEY
+fi
+
+if [ -n "$MAIL_ENV_PASSWORD_FILE}" ]; then
+  MAIL_ENV_PASSWORD=$(cat "$MAIL_ENV_PASSWORD_FILE")
+  export MAIL_ENV_PASSWORD
+fi
+
+if [ -n "${MYSQL_PASSWORD_FILE}" ]; then
+  MYSQL_PASSWORD=$(cat "$MYSQL_PASSWORD_FILE")
+  export MYSQL_PASSWORD
+fi
+
 # fix key if needed
 if [ -z "$APP_KEY" ]
 then
